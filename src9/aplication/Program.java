4f92ed19a6b349c97454353d89c40f065cbd07fb
pbelem71 <pedrohenriquevale3202@gmail.com>
2024-09-15 00:06:26 -0300

    Feature
    Learning how to use constructors, getters and setters methods

--- src9/aplication/Program.java
+++ src9/aplication/Program.java
@@ -1,5 +1,45 @@
 package aplication;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product1;
+
 public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the product data: ");
+		System.out.println("Name: ");
+		String name = sc.nextLine();
+		System.out.println("Price: ");
+		Double price = sc.nextDouble();
+		System.out.println("Quantity in stock: ");
+		int quantity = sc.nextInt();
+
+		Product1 product = new Product1(name, price, quantity);
+
+		System.out.println();
+		System.out.println("Product data: " + product);
+
+		System.out.println("Enters the number of products to be added in stock: ");
+		quantity = sc.nextInt();
+		product.addProducts(quantity);
+
+		System.out.println();
+		System.out.println("Updated data: " + product);
+
+		System.out.println("Enters the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+
+		System.out.println();
+		System.out.println("Updated data: " + product);
+
+		sc.close();
+	}
 
 }
--- src9/entities/Product1.java
+++ src9/entities/Product1.java
@@ -2,4 +2,59 @@ package entities;
 
 public class Product1 {
 
+	private String name;
+	private Double price;
+	private int quantity;
+
+	public Product1() {
+	}
+
+	public Product1(String name, Double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public Product1(String name, Double price) {
+		this.name = name;
+		this.price = price;
+//		por padrão quantity inicia com 0, mas tbm poderia ser feito um quantity = 0;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public Double totalValueInStock() {
+		return price * quantity;
+	}
+
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+
+	public String toString() {
+		return name + ", $ " + String.format("%.2f", price) + ", " + quantity + " units, Total: $ "
+				+ String.format("%.2f", totalValueInStock());
+	}
 }
