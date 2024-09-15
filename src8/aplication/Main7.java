2f8637b6976ed262556b0785a3ebd0ff6b78045b
pbelem71 <pedrohenriquevale3202@gmail.com>
2024-09-14 23:01:25 -0300

    Update Main7.java

--- src8/aplication/Main7.java
+++ src8/aplication/Main7.java
@@ -6,20 +6,20 @@ import java.util.Scanner;
 import util.Calculator1;
 
 public class Main7 {
-	
+
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		Calculator1 calc = new Calculator1();
 
-		System.out.println("Enter the Dollar price: ");
-		calc.dollarPrice = sc.nextDouble();
-		System.out.println("Enter the value in Dollars: ");
-		calc.dollarValues = sc.nextDouble();
+		Double dollarPrice, dollarValues;
+
+		System.out.println("What is the Dollar price?");
+		dollarPrice = sc.nextDouble();
+		System.out.println("How many dollars will be bought?");
+		dollarValues = sc.nextDouble();
 		
-		
-		
+		System.out.println("Amount to be paid in reais = " + (Calculator1.currentConverter(dollarPrice, dollarValues)
+		+ (Calculator1.currentConverter(dollarPrice, dollarValues) * 0.06)));
 		
 		sc.close();
 	}
